ae733b96280c3db00330876c318bc0bdb56add98
Hisham Ahamed <57804198+hisham-ahamed@users.noreply.github.com>
2023-06-12 22:37:15 +0300

    Create hsmtxt.sh

---
+++ hsmtxt.sh
@@ -0,0 +1,1 @@
+apt purge task-desktop hyphen-en-us libglu1-mesa libreoffice-* libu2f-udev mythes-en-us x11-apps x11-session-utils xinit xorg xserver-* desktop-base task-german task-german-desktop totem gedit gedit-common gir1.2-* gnome-* gstreamer* sound-icons speech-dispatcher totem-common xserver-* xfonts-* xwayland gir1.2* gnome-*
