620256d051cb6e32f5921b42191d4410fd76e59b
Hisham Ahamed <57804198+hisham-ahamed@users.noreply.github.com>
2024-02-11 10:37:03 +0300

    Update migrateParser.ps1

--- migrateParser.ps1
+++ migrateParser.ps1
@@ -1,22 +1,43 @@
-# Get-AzContext -ListAvailable
 # Set the source workspace
 Set-AzContext -Subscription "HMC-Hub"
-$ResourceGroup = "rg-sec-hub-weu-001"
-$WorkspaceName = "log-hub-weu-001"
+$SourceResourceGroup = "rg-sec-hub-weu-001"
+$SourceWorkspaceName = "log-hub-weu-001"
 
 # Only export saved queries from these categories
 $Categories = ("CISCO")
 
-$ExportedSearches = (Get-AzOperationalInsightsSavedSearch -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName).Value.Properties | Where-Object { $Categories -contains $_.Category }
+$ExportedSearches = (Get-AzOperationalInsightsSavedSearch -ResourceGroupName $SourceResourceGroup -WorkspaceName $SourceWorkspaceName).Value.Properties | Where-Object { $Categories -contains $_.Category }
 
 # Set the destination workspace
 Set-AzContext -Subscription "HMC-Hub-QC"
-$ResourceGroup = "rg-sec-hub-qc-001"
-$WorkspaceName = "log-hub-qc-001"
+$DestResourceGroup = "rg-sec-hub-qc-001"
+$DestWorkspaceName = "log-hub-qc-001"
 
 # Import Saved Searches as Functions
 foreach ($search in $ExportedSearches) {
-    $id = $search.Category + "|" + $search.DisplayName
-    New-AzOperationalInsightsSavedSearchV2 -Force -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName -SavedSearchId $id -DisplayName $search.DisplayName -Category $search.Category -Query $search.Query -Version $search.Version -IsFunction $true
+    # Create a function in the destination workspace
+    $functionName = $search.DisplayName
+    $functionQuery = $search.Query
+    $function = @{
+        "Content" = $functionQuery
+    }
+    $functionId = (New-AzResource -ResourceType Microsoft.OperationalInsights/workspaces/functions `
+                                    -ResourceGroupName $DestResourceGroup `
+                                    -WorkspaceName $DestWorkspaceName `
+                                    -Name $functionName `
+                                    -Properties $function).Properties.id
+    
+    # Assign the query to the function
+    $savedSearch = @{
+        "Properties" = @{
+            "Category" = "Custom"
+            "Query" = $functionId
+        }
+    }
+    $savedSearchId = $search.Category + "|" + $search.DisplayName
+    New-AzResource -ResourceType Microsoft.OperationalInsights/workspaces/savedSearches `
+                   -ResourceGroupName $DestResourceGroup `
+                   -WorkspaceName $DestWorkspaceName `
+                   -Name $savedSearchId `
+                   -Properties $savedSearch
 }
-
